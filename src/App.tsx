90a1f59e3e4108cf893bc3d471f1ac85677342a1
subash <subashsuni55@gmail.com>
2023-11-30 20:14:20 +0530

    fix : fixed the image issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { Fragment, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Fragment } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Fragment>
